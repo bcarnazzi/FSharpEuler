bf10cd4a649e8c4a23857fc9a2d9d1d42399bf74
Bruno Carnazzi <bcarnazzi@gmail.com>
2023-04-11 19:24:44 +0400

    added pr004

---
+++ pr004/Program.fs
@@ -0,0 +1,14 @@
+﻿module pr004
+
+let solve =
+    seq {
+        for a in 100..999 do
+            yield! seq { for b in a..999 -> a * b |> bigint }
+    }
+    |> Seq.filter eulerlib.isPalindromic
+    |> Seq.max
+
+[<EntryPoint>]
+let main _ =
+    printfn $"{solve}"
+    0
