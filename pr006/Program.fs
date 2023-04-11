afd5329d4d68c02fe59e7a49452c2c74b7fec3cb
Bruno Carnazzi <bcarnazzi@gmail.com>
2023-04-11 21:49:10 +0400

    added pr006

---
+++ pr006/Program.fs
@@ -0,0 +1,11 @@
+﻿module pr006
+
+let solve (n : uint64) : uint64 =
+    let a = seq { 1UL .. n } |> Seq.map (fun n -> n * n) |> Seq.sum
+    let b = seq { 1UL .. n } |> Seq.sum |> (fun x -> x * x)
+    b - a
+
+[<EntryPoint>]
+let main _ =
+    printfn $"{solve 100UL}"
+    0
