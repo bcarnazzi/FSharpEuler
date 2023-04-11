8d44db81dc4d68a39a64b32b8713177aede75ee7
Bruno Carnazzi <bcarnazzi@gmail.com>
2023-04-12 00:04:52 +0400

    pr009 indentation

--- pr009/Program.fs
+++ pr009/Program.fs
@@ -1,17 +1,18 @@
 ﻿module pr009
 
-let isPythagoreanTriplet a b c =
-    a < b && b < c && a * a + b * b = c * c
+let isPythagoreanTriplet a b c = a < b && b < c && a * a + b * b = c * c
 
-let solve (n : uint64) =
+let solve (n: uint64) =
     seq {
         for a in 1UL .. n do
-            for b in a .. n do
+            for b in a..n do
                 let c = n - a - b
+
                 if a + b < n && isPythagoreanTriplet a b c then
                     yield a * b * c
-                
-    } |> Seq.max
+
+    }
+    |> Seq.max
 
 [<EntryPoint>]
 let main _ =
