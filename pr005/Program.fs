4292a069d32ff8e2c1931ce91c9c34be91123761
Bruno Carnazzi <bcarnazzi@gmail.com>
2023-04-11 22:56:42 +0400

    pr005 curryfication

--- pr005/Program.fs
+++ pr005/Program.fs
@@ -1,7 +1,7 @@
 ﻿module pr005
 
 let solve (n : bigint ) : bigint =
-    seq { 1I .. n } |> Seq.fold (fun acc elem -> eulerlib.lcm acc elem) 1I
+    seq { 1I .. n } |> Seq.fold eulerlib.lcm 1I
 
 [<EntryPoint>]
 let main _ =
